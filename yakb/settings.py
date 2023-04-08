af43e003fb3049f23383db2bc453da7e4da5f79c
Matthias Roussel <matthias.roussel01@gmail.com>
2023-04-08 23:09:16 +0200

    chore: Setup Tetra and Poetry.

--- yakb/settings.py
+++ yakb/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'tetra',
     'django.contrib.staticfiles',
 ]
 
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'tetra.middleware.TetraMiddleware'
 ]
 
 ROOT_URLCONF = 'yakb.urls'
