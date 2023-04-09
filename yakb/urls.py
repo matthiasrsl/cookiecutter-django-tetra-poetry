793e76f70895e5025cfce4e94e7fcea4f3120c15
Matthias Roussel <matthias.roussel01@gmail.com>
2023-04-09 22:04:40 +0200

    chore: Added Pylint and black.

--- yakb/urls.py
+++ yakb/urls.py
@@ -19,6 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tetra/', include('tetra.urls')),
+    path("admin/", admin.site.urls),
+    path("tetra/", include("tetra.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
