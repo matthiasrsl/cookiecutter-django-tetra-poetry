35ba6514a19ecc430cd41cd0e28df86c55bd4ada
Matthias Roussel <matthias.roussel01@gmail.com>
2023-04-22 03:33:45 +0200

    feat: Added pre-commit to the post-gen hook.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,5 +2,9 @@
 npm install esbuild
 poetry install
 poetry run python manage.py migrate
+git init --initial-branch=main
 poetry run pre-commit install
-poetry run pre-commit run --all-files
+poetry run pre-commit install --hook-type commit-msg
+poetry run pre-commit autoupdate
+git add .
+git cm -m "chore: Initial commit."
