eba655de39a77152a544edc791f7229c885b4b23
Matthias Roussel <matthias.roussel01@gmail.com>
2023-04-23 02:00:15 +0200

    feat: Added bandit security checker.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -8,6 +8,6 @@ poetry run pre-commit install --hook-type commit-msg
 poetry run pre-commit autoupdate
 git add .
 git cm -m "chore: Initial commit."
-echo -e "\n\n\033[1;32mProject set up successfully! 🎉\033[0m\n"
+echo -e "\n\n\033[1;32mProject successfully set up! 🎉\033[0m\n"
 echo -e "You can now run the development server with:\n $ cd {{cookiecutter.__project_slug}}\n $ poetry shell\n $ python manage.py runserver"
 echo -e "\n\033[1;31mIMPORTANT: Don't forget to change Django's SECRET_KEY in the .env file!\033[0m\n"
