a289ae0f9e4773408c5c5e9e0018a89f4649371d
Matthias Roussel <matthias.roussel01@gmail.com>
2023-07-08 00:53:30 +0200

    fix(environ): Env file was not loaded.

--- {{cookiecutter.__project_slug}}/{{cookiecutter.__project_slug}}/settings.py
+++ {{cookiecutter.__project_slug}}/{{cookiecutter.__project_slug}}/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(DJANGO_DEBUG=(bool, False))
 
+environ.Env.read_env(BASE_DIR / ".env")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
