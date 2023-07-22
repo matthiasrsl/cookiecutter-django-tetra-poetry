53be5f5041eaa2aee15d1d4377133fa68fd762e8
Matthias Roussel <matthias.roussel01@gmail.com>
2023-07-22 02:22:09 +0200

    fix: template location

--- {{cookiecutter.__project_slug}}/{{cookiecutter.__main_app_slug}}/views.py
+++ {{cookiecutter.__project_slug}}/{{cookiecutter.__main_app_slug}}/views.py
@@ -4,4 +4,4 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
     """Home Page of the app."""
 
-    template_name = "home.html"
+    template_name = "app/home.html"
