71a8fe023511622e93a1863d5675a3e69aa0c788
Matthias Roussel <matthias.roussel01@gmail.com>
2023-07-23 23:41:29 +0200

    fix: Homepage template name was hardcoded.

--- {{cookiecutter.__project_slug}}/{{cookiecutter.__main_app_slug}}/views.py
+++ {{cookiecutter.__project_slug}}/{{cookiecutter.__main_app_slug}}/views.py
@@ -4,4 +4,4 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
     """Home Page of the app."""
 
-    template_name = "app/home.html"
+    template_name = "{{cookiecutter.__main_app_slug}}/home.html"
